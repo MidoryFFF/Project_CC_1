6100f3e20082457999838c7abeef1e8c299cee0c
MidoryFFF <127252583+MidoryFFF@users.noreply.github.com>
2025-10-02 07:54:09 +0300

    Clining out excess

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import os
-isRunning = True
+import socket
+
 username = os.getlogin()
-pathFromMainRunner = "<path>"
-while isRunning:
-    terminalInput = input(f"{username}:{pathFromMainRunner} ")
-    command = terminalInput.split()[0]
-    match command:
-        case "exit":
-            isRunning = False
-        case "echo":
-            print(terminalInput[5:])
-        case "ls":
-            print("ls")
-        case "cd":
-            print("cd <path>")
-        case _:
-            print("Command not found")
+hostname = socket.gethostname()
+pathToMainRunner = os.path.abspath(__file__)
+pathToVFSDerectory = os.path.dirname(__file__) + "\\VFS"
+corretPath = pathToMainRunner
+
+def PathTransformer():
+    #Если файл в котором находиться пользователь основной, то он будет отображаться как ~$
+    if (corretPath == pathToMainRunner):
+        return "~$"
+    else:
+        return corretPath
 
 def FindArgsInBrackets(_string):
     if (_string.find('"').count() == 2):
         return _string[1::-1].split()
     else:
         return ""
+
+def main():
+    isRunning = True
+    print("Path to VFS: " + pathToVFSDerectory)
+    print("Path to main: " + pathToMainRunner)
+    while isRunning:
+        path = PathTransformer()
+        terminalInput = input(f"{username}@{hostname}:{path} ")
+        command = terminalInput.split()[0]
+        match command:
+            case "exit":
+                isRunning = False
+            case "echo":
+                print(terminalInput[5:])
+            case "ls":
+                print("ls")
+            case "cd":
+                print("cd <path>")
+            case _:
+                print("Command not found")
+
+if (__name__ == "__main__"):
+    main()
